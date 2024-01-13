995cd4140f3e74d1259fd0e0d7315787a047aedc
gisellitiWeb <oreodemorango.contato@gmail.com>
2024-01-13 13:50:45 -0300

    Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia smartNokia = new Nokia("082849124", "Nokia", "777777777", 70);
+smartNokia.Ligar();
+
+Iphone smartIphone = new Iphone("91637853", "Iphone", "627272713", 85);
+smartIphone.ReceberLigacao();
+
+smartNokia.InstalarAplicativo("GitHub");
+smartIphone.InstalarAplicativo("Instagram");
